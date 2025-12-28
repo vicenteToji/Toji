b73a98fce4ee4bc17e2649c65c4b7afba02183af
vicente <u1646291625@gmail.com>
2025-12-28 14:51:10 +0100

    Instalación de dependencias iniciales, primer app.mjs

---
+++ web/app.mjs
@@ -0,0 +1,25 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Servidor de Gimnasio Toji funcionando ');
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor en ${PORT}`);
+});
